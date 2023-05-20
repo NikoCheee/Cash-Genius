c9d48428622f4ea88a23c2fd26d53a7ab74cad78
NikoCheee <nikita3ch@gmail.com>
2023-05-20 23:41:34 +0300

    Add Meta class for correct view in admin panel

--- site_app/models.py
+++ site_app/models.py
@@ -12,6 +12,10 @@ class Category(models.Model):
     english_name = models.CharField(max_length=100)
     slug = models.SlugField()
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
